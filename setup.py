073eaea6141abf4b8e498387fb0bef470f8161f4
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-15 12:31:27 +0800

    alvistack/4.9.1
        git clean -xdf
        tar zcvf ../python-platformdirs_4.9.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-platformdirs.spec ../python-platformdirs_4.9.1-1.spec
        cp ../python*-platformdirs*4.9.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/platformdirs-platformdirs-4.9.1/
        rm -rf ../python*-platformdirs*4.9.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='platformdirs',
+    version='4.9.1',
+    description='A small Python package for determining appropriate platform-specific dirs, e.g. a `user data dir`.',
+    maintainer_email='Bernát Gábor <gaborjbernat@gmail.com>, Julian Berman <Julian@GrayVines.com>, Ofek Lev <oss@ofek.dev>, Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'platformdirs',
+    ],
+    package_dir={'': 'src'},
+)
